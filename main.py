79a0514e0611bdc3c24bd92f162f4a14f6d51a8d
DaviPires10 <davipiresalvesdacunha2@gmail.com>
2024-03-12 21:28:52 -0300

    Lê ai cara

--- main.py
+++ main.py
@@ -1,47 +1,66 @@
-import os
 from datetime import datetime
 import json
+import os
 
 
 """
- Get all the files on initialize computer
- Check their existence time
- Remove from server if existence time > 37 days (a month and a week) 
- 
- Check if there are new files 
- Add to server
- Repeat same process
- """
+Instructions:
 
+  Get all the files on initialize computer
+  Check their existence time
+  Remove from server if existence time > 37 days (a month and a week) 
+  
+  Check if there are new files 
+  Add to server
+  Repeat same process
+  
+"""
 
-path : str = "C:\\Users\\davip\\Animes"
-server : str = "./all_files.json"
-all_files : dict[str, int] = {}
-
-def get_all_files() -> None:
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file = os.path.join(root, file)
-            all_files[file] = get_existence_time(file)
-
-def get_existence_time(file : str) -> int:
-    return (datetime.now() - datetime.fromtimestamp(os.path.getmtime(file))).days
-
-
-def add_new_file_server(file : str):          
-    with open(server) as file:
-        files_in_server : dict = json.load(server)
+class Name:
+    path : str = "Z:\\"
+    server = "Professores"
+    profs : list[str] = [
+        'Aline', 
+        'Anderson', 
+        'Edmeire',
+        'Michele',
+        'Reinaldo V',
+        'Ricardo',
+        'Tais',
+        'Tati',
+        'Wellington'
+        ]
     
-        if file not in files_in_server.keys():
-            
-            # Faça a lógica de colocar o arquivo em si na próprio server
-            json.dump(all_files, file)
+    def __init__(self):
+        for prof in self.profs:
+            prof_json = self.get_all_files(prof)
+            with open(f"{self.server}\\{prof}.json", "w") as file:
+                json.dump(prof_json, file, ensure_ascii= False)
+    
+    def get_all_files(self, prof : str) -> dict:
+        prof_json : dict = {}
         
+        for root, folders, files in os.walk(f"{self.path}Prof {prof}"):
+           
+          for folder in folders:
+                prof_json[folder] = {folder: dict()}
+                
+          for file in files:
+              
+              file = os.path.join(root, file)
+              folder = file.split("\\")[-2]
+              prof_json[folder][file] = self.get_existence_time(file)
+                
+        return prof_json
+    
+    def get_existence_time(file : str) -> int:
+        return (datetime.now() - datetime.fromtimestamp(os.path.getmtime(file))).days
 
-            
-#  get_all_files()
-
-  
- 
- 
-  
+    def add_new_file_server(self, file : str):          
+        with open(self.server) as file:
+            files_in_server : dict = json.load(self.server)
+        
+            if file not in files_in_server.keys(): {}
+                
+                # Faça a lógica de colocar o arquivo em si na próprio server
+                # json.dump(all_files, file, ensure_ascii= False)
